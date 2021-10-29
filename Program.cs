16391cd1913516e6f59834e35f5d679adfb427f0
William-italia <williamitalia70@outlook.com>
2021-10-29 07:51:39 -0300

    exercicio equação do segundo grau

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExercicioEquacao2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      double a, b, c, delta, x1, x2;
+
+      Console.WriteLine("- - - Equação do segundo grau - - -");
+      Console.WriteLine("Digite o valor das letras a, b, c.\n");
+      Console.Write("(a): ");
+      a = double.Parse(Console.ReadLine());
+      Console.Write("(b): ");
+      b = Double.Parse(Console.ReadLine());
+      Console.Write("(c): ");
+      c = Double.Parse(Console.ReadLine());
+
+      delta = Math.Pow(b, 2) - 4 * a * c;
+      x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+      x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+
+      if (a == 0)
+      {
+        Console.WriteLine("Não é uma equação de segundo grau!");
+      }
+      else if (delta < 0)
+      {
+        Console.WriteLine($"Como delta é igual a {delta:N2} a equação não possui raízes reais!");
+      }
+      else
+      {
+        Console.WriteLine($"\nO valor de delta é: {delta}");
+        Console.WriteLine($"x¹ = {x1:N2}");
+        Console.WriteLine($"x² = {x2:N2}");
+      }
+        
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ExercicioEquacao2
 Calcule as raízes de uma equação de segundo grau, utilizando o método de Bhaskara.
+## Exemplos
+ex..:
+```
+- - - Equação do segundo grau - - -
+Digite o valor das letras a, b, c.
+
+(a): 3
+(b): -7
+(c): 2
+
+O valor de delta é: 25
+x¹ = 2,00
+x² = 0,33
+```
+ex..:
+```
+(a): 0
+(b): 2
+(c): 45
+Não é uma equação de segundo grau!
+```
+ex..:
+```
+(a): 1  
+(b): 2
+(c): 3
+Como delta é igual a -8,00 a equação não possui raízes reais!
+```
